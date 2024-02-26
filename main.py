de05340539919cb707d8dfab91d773c0e2185596
ISeeBugsEverywhere <menulio.svytuokle@gmail.com>
2024-02-26 15:12:31 +0200

    VScode

---
+++ main.py
@@ -0,0 +1,19 @@
+#standartiniai moduliai
+import numpy as np
+import numpy.polynomial.polynomial as poly
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from datetime import datetime
+from dateutil import parser
+import mysql.connector as cnt
+#import panel as pn
+#pn.extension()
+import seaborn as sns
+import os, sys
+print(os.getcwd())
+fontsize=16 #for all entries
+titlesize=24
+labelsize=16
+fwidth=6
+fheight=4
